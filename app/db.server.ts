f3c9392d8e8b54b9621b05dfb20e7288f4c02759
Mahfuzsds <mahfuzar@smartdatasoft.net>
2024-06-16 23:36:05 +0600

    useNewUrlParser issue red on console. fix

--- app/db.server.ts
+++ app/db.server.ts
@@ -9,10 +9,7 @@ const connectToMongoDB = async () => {
   }
 
   try {
-    await mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
